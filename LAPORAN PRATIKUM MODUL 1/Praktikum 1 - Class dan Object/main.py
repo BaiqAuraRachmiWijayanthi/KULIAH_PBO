67cda0c55335aca0dc8d0c91bd0e3938ccf012b1
Baiq Aura <aurarchmiwjynthi@gmail.com>
2025-10-13 19:27:50 +0800

    Add files via upload

---
+++ LAPORAN PRATIKUM MODUL 1/Praktikum 1 - Class dan Object/main.py
@@ -0,0 +1,22 @@
+#pratikum 1 : Class  dan Object
+class Hero: 
+    pass
+
+# Proses penciptaan object
+hero_1 = Hero() 
+hero_2 = Hero() 
+
+# Memberikan atribut baru name untuk masing-masing object
+hero_1.name = "Goku"
+hero_2.name = "Vegeta"
+
+# Menambahkan atribut power ke object
+hero_1.power = 100
+hero_2.power = 90
+
+# Menampilkan nilai atribut name dari kedua object
+print(hero_1.name)
+print(hero_2.name)  
+
+# Menampilkan atribut apa saja yang dimilii object dalam bentuk dictionary (key = nama atribut,value = nilainya).
+print(hero_1.__dict__) 
